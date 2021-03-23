debb38237fe633e150bf1bcbf87861687d5f52e7
ddatta <ddatta@vt.edu>
2021-03-23 10:26:46 -0400

    Changes

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='evalSemanticSeg',
+      version='0.0.1',
+      description='Evaluate semantic segmentation for cutomized data, for models trained on cityscapes',
+      url='https://github.com/ddatta-DAC/evalSemanticSeg',
+      author='Debanjan Datta',
+      author_email='ddatta@cs.vt.com',
+      license='MIT',
+      packages=['evalSemanticSeg'],
+      install_requires=[
+          'numpy',
+      ]
+      zip_safe=False)
